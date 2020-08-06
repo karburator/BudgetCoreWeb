c41139f3b42810f1e7ebd83db29811bb3a128f0c
karburator <dav_26rus@mail.ru>
2020-08-06 21:36:20 +0300

    fix model

--- src/app/model/Good.ts
+++ src/app/model/Good.ts
@@ -1,14 +1,16 @@
+import { Product } from './Product';
+
 export class Good {
-  Id: number;
-  Name: string;
-  Price: number;
-  Quantity: number; // double
-  Nds18: number;
-  Nds10: number;
-  Nds0: number;
-  NdsNo: number;
-  NdsCalculated18: number;
-  NdsCalculated10: number;
-  Sum: number;
-  ReceiptId: number;
+  id: number;
+  product: Product;
+  price: number;
+  quantity: number; // double
+  nds18: number;
+  nds10: number;
+  nds0: number;
+  ndsNo: number;
+  ndsCalculated18: number;
+  ndsCalculated10: number;
+  sum: number;
+  receiptId: number;
 }
